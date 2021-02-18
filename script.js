4bd8aadddaa0ad684904c785e139e59dfa723d6a
Ilya <ilyarahl@gmail.com>
2021-02-18 20:44:30 +0300

    add index.html and first script

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+let numberOfFilms = prompt('How many movies have you seen?','');
+
+let personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let firstFilm = prompt('One of the last movie have you seen?', '');
+let firstFilmRate = prompt('How do you rate it?', '');
+let secondFilm = prompt('One of the last movie have you seen?', '');
+let secondFilmRate = prompt('How do you rate it?', '');
+
+personalMovieDB.movies[firstFilm] = firstFilmRate;
+personalMovieDB.movies[secondFilm] = secondFilmRate;
+
+console.log(personalMovieDB.movies);
+
+
+
